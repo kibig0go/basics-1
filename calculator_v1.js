98a32c4401d287f0cc9d0efa20fe4e2f7469ceeb
Kitaev Boris <borkit2000@mail.ru>
2022-08-13 16:01:40 +0300

    unnecessary variables removed

--- calculator_v1.js
+++ calculator_v1.js
@@ -11,10 +11,6 @@ function calc(action, a, b) {
     }
 }
 
-let value1 = calc('add', 1, 2);
-let value2 = calc('multi', 1, 2);
-let value3 = calc('subtract', 3, 2);
-
-console.log('value1 = ' + value1);
-console.log('value2 = ' + value2);
-console.log('value3 = ' + value3);
+console.log('value1 = ' + calc('add', 1, 2));
+console.log('value2 = ' + calc('multi', 1, 2));
+console.log('value3 = ' + calc('subtract', 3, 2));
